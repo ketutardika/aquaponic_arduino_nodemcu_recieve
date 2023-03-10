2c9f035e4c48dc42efc9d1d8bd61fcc9524523ed
Ketut Ardika <ardikaimb@gmail.com>
2023-03-11 01:56:59 +0800

    add reading esp

--- aquamonia_arduino_nodemcu_recieve.ino
+++ aquamonia_arduino_nodemcu_recieve.ino
@@ -8,20 +8,18 @@
 #include "recieve_send_data.h"
 
 //D6 = Rx & D5 = Tx
-SoftwareSerial nodemcu(D6, D5);
-
-float temperature_sensor, humidity_sensor, tds_sensor, turbidity_sensor, water_temp_sensor, ph_sensor;
+SoftwareSerial SerialMega(D6, D5);
 
 const int interval = 3600000; // Waktu interval dalam milidetik (3 jam = 3 x 60 x 60 x 1000)
 unsigned long previousMillis = 0;
 
 void setup() {
-  Serial.begin(115200);
-  setup_sendData();
-  Serial1.begin(9600);
-  nodemcu.begin(9600);
+  Serial.begin(115200);  
+  SerialMega.begin(9600);
   setup_wifi_manager();
   setup_esp_server();
+  //setup_sendData();
+  Serial.println("ESP Start");
   delay(5000);
 }
 
@@ -38,47 +36,52 @@ String bacaDariEEPROMMainMaster(int addr) {
 
 void loop() { 
   handle_esp_server();
-  if (nodemcu.available()) 
-  {
-  StaticJsonDocument<300> doc;
-  DeserializationError error = deserializeJson(doc, nodemcu);
-  // Check for errors
-  if (error) {
-    Serial.print(F("deserializeJson() failed: "));
-    Serial.println(error.c_str());
-    return;
-  }
-
-  float temperature_sensor = doc["temperature_sensor"];
-  float humidity_sensor = doc["humidity_sensor"];
-  float tds_sensor = doc["tds_sensor"];
-  float turbidity_sensor = doc["turbidity_sensor"];
-  float water_temp_sensor = doc["water_temp_sensor"];
-  float ph_sensor = doc["ph_sensor"];
-
-  Serial.println(temperature_sensor);
-  Serial.println(humidity_sensor);
-  Serial.println(tds_sensor);
-  Serial.println(turbidity_sensor);
-  Serial.println(water_temp_sensor);
-  Serial.println(ph_sensor);
-  delay(10000);
-  } else {
-    Serial.println("Node Not Available");
-    delay(10000);
-  }
-  
   String intervalhour = bacaDariEEPROMMainMaster(0);
   int intervalhourResult = intervalhour.toInt();
   int intervalTotal = interval * intervalhourResult;
 
   if (!intervalhourResult || intervalhourResult==1){
-    intervalTotal = 60000;
+    intervalTotal = 10000;
   }
 
   unsigned long currentMillis = millis();
   if (currentMillis - previousMillis >= intervalTotal) {
-    handle_sendData();
+    readSerials();
+    //handle_sendData();
     previousMillis = currentMillis;
+  }
+}
+
+void readSerials() {
+  //Menunggu sampai ada data yang diterima dari Arduino
+  while (!SerialMega.available()) {
+    delay(100);
+  }
+  // Membaca data dari Arduino
+  String jsonString = "";
+  while (SerialMega.available()) {
+    jsonString += (char)SerialMega.read();
+  }
+  //Parsing data JSON
+  StaticJsonDocument<200> doc;
+  DeserializationError error = deserializeJson(doc, SerialMega);
+  if (!error){
+    //Mengambil nilai dari objek JSON
+    String tds = doc["tds_sensor"].as<String>();
+    String ph = doc["ph_sensor"].as<String>();
+
+    float temperature_sensor = doc["temperature_sensor"].as<float>();
+    float humidity_sensor = doc["humidity_sensor"].as<float>();
+    float tds_sensor = doc["tds_sensor"].as<float>();
+    float turbidity_sensor = doc["turbidity_sensor"].as<float>();
+    float water_temp_sensor = doc["water_temp_sensor"].as<float>();
+    float ph_sensor = doc["ph_sensor"].as<float>();
+
+    Serial.println(temperature_sensor);
+    Serial.println(humidity_sensor);
+    Serial.println(tds_sensor);
+    Serial.println(turbidity_sensor);
+    Serial.println(water_temp_sensor);
+    Serial.println(ph_sensor);
   }
 }
